d2ff4400bd798358e0d7820d59bbd6c9eb4758bc
pillow <waysorrypub@gmail.com>
2026-04-01 18:41:07 +0800

    添加test.h头文件

---
+++ test/test.h
@@ -0,0 +1,24 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#define ANSI_FG_GREEN "\33[1;32m"
+#define ANSI_FG_RED "\33[1;31m"
+#define ANSI_NONE "\33[0m"
+
+#define RUN_TEST(fn)                                                           \
+        do {                                                                   \
+                fn();                                                          \
+                printf(ANSI_FG_GREEN "[PASS]: %s:%d %s\n" ANSI_NONE, __FILE__, \
+                       __LINE__, __func__);                                    \
+        } while (0)
+
+#define ASSERT(cond)                                                           \
+        do {                                                                   \
+                if (!(cond)) {                                                 \
+                        printf(ANSI_FG_RED "[ASSERT] ASSERT failed: " #cond    \
+                                           " (%s:%d %s)\n" ANSI_NONE,          \
+                               __FILE__, __LINE__, __func__);                  \
+                }                                                              \
+        } while (0)
+
+#endif
